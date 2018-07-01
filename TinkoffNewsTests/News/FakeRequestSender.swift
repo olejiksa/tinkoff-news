9ae8379224ad9bc0ddec9a876c2eced3dcf28f6f
Oleg <quillaur@outlook.com>
2018-07-01 16:04:13 +0300

    News service tests were rewritten completely

---
+++ TinkoffNewsTests/News/FakeRequestSender.swift
@@ -0,0 +1,41 @@
+//
+//  FakeRequestSender.swift
+//  TinkoffNewsTests
+//
+//  Created by Oleg Samoylov on 01/07/2018.
+//  Copyright © 2018 Oleg Samoylov. All rights reserved.
+//
+
+import Foundation
+@testable import TinkoffNews
+
+protocol Nameable {
+    var resourseName: String? { get set }
+}
+
+class FakeRequestSender: IRequestSender, Nameable {
+    var resourseName: String?
+    
+    func send<Parser>(config: RequestConfig<Parser>, completion: @escaping (Result<Parser.Model>) -> ()) where Parser: IParser {
+        guard let resouseNameUnwrapped = resourseName else {
+            completion(.error("Resource name is nil."))
+            return
+        }
+        
+        if let path = Bundle(for: type(of: self)).path(forResource: resouseNameUnwrapped, ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                guard let parsedModel: Parser.Model = config.parser.parse(data: data) else {
+                    completion(.error("Received data cannot be parsed."))
+                    return
+                }
+                
+                completion(.success(parsedModel))
+            } catch {
+                completion(.error(error.localizedDescription))
+            }
+        } else {
+            completion(.error("Not found: \(resouseNameUnwrapped)"))
+        }
+    }
+}
---
+++ TinkoffNewsTests/News/NewsTests.swift
@@ -0,0 +1,91 @@
+//
+//  NewsTests.swift
+//  TinkoffNewsTests
+//
+//  Created by Oleg Samoylov on 30/06/2018.
+//  Copyright © 2018 Oleg Samoylov. All rights reserved.
+//
+
+import XCTest
+@testable import TinkoffNews
+
+class NewsTests: XCTestCase {
+    
+    /// Possible page numbers as sample
+    private let pageNumbers = [.min, -7, 0, 7, 20, .max]
+    
+    /// Service layer object to test
+    private var newsService: (IFeedService & IPostService)!
+    
+    /// Core layer object
+    private var requestSender: (IRequestSender & Nameable)!
+    
+    // MARK: - Setting up of test environment
+    
+    override func setUp() {
+        super.setUp()
+        
+        requestSender = FakeRequestSender()
+        newsService = NewsService(requestSender: requestSender)
+    }
+    
+    override func tearDown() {
+        newsService = nil
+        requestSender = nil
+        
+        super.tearDown()
+    }
+    
+    // MARK: - Tests
+    
+    func testNewsFeed() {
+        requestSender.resourseName = "FakeNewsFeed"
+        
+        var feedExpectations = [XCTestExpectation]()
+        
+        for page in pageNumbers {
+            let postExpectation = XCTestExpectation(description: "News post on page \(page) has been loaded")
+            feedExpectations.append(postExpectation)
+            
+            newsService.getNewsFeed(page: page) { feed, error in
+                XCTAssertTrue(error == nil || error?.description == "Index out of bounds")
+                
+                if error == nil {
+                    XCTAssertNotNil(feed)
+                }
+                
+                postExpectation.fulfill()
+            }
+        }
+        
+        wait(for: feedExpectations, timeout: 8)
+    }
+    
+    func testNewsPost() {
+        requestSender.resourseName = "FakeNewsPost"
+        
+        let expectation = XCTestExpectation(description: "News post has been loaded")
+        let expectationModel = (id: "9746",
+                                contentStartsWith: "Москва, Россия")
+        
+        newsService.getNewsPost(id: expectationModel.id) { post, error in
+            guard let postUnwrapped = post else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertNil(error)
+            
+            XCTAssertEqual(postUnwrapped.id, expectationModel.id)
+            
+            let decodedString = String(htmlEncodedString: postUnwrapped.content)
+            let prefixedDecodedString = decodedString?.prefix(expectationModel.contentStartsWith.count).description
+            XCTAssertEqual(prefixedDecodedString, expectationModel.contentStartsWith)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+}
